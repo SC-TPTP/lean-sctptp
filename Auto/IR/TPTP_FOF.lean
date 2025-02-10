0543c26804ed84784a3185da899c1f67a375ed9d
Auguste Poiroux <augustepoiroux@gmail.com>
2025-02-10 19:31:35 +0100

    Fix bilinear applications export

--- Auto/IR/TPTP_FOF.lean
+++ Auto/IR/TPTP_FOF.lean
@@ -46,28 +46,55 @@ def transLamBaseTerm : LamBaseTerm → Except String String
 | .forallEI _ => .error "transLamBaseTerm :: forallEI should not occur here"
 | .existEI _  => .error "transLamBaseTerm :: existEI should not occur here"
 | .iteI _     => .error "transLamBaseTerm :: iteI should not occur here"
-| .eq _       => .ok "(=)"
-| .forallE _  =>
-    .ok "($true)"  -- if the bound sort is empty, we output $true
-| .existE _   =>
-    .ok "($false)" -- similarly for exists
-| .ite _      => .ok "($ite)"  -- treat ite op abstractly
+| .eq _       => .error "transLamBaseTerm :: eq should not occur here"
+| .forallE _  => .ok "($true)"
+| .existE _   => .ok "($false)"
+| .ite _      => .ok "($ite)"
+
+def isBiapp : LamBaseTerm → Bool
+| .eq _ => true
+| .pcst .and => true
+| .pcst .or => true
+| .pcst .imp => true
+| .pcst .iff => true
+| _ => false
+
+def biappOp : LamBaseTerm → String
+| .eq _ => "="
+| .pcst .and => "&"
+| .pcst .or => "|"
+| .pcst .imp => "=>"
+| .pcst .iff => "<=>"
+| _ => ""
 
 partial def transLamTerm (t : LamTerm) (lctx := 0) : Except String String :=
   match t with
   | .atom n      => .ok s!"t_a{n}"
   | .etom n      => .ok s!"t_e{n}"
-  | .base b      => transLamBaseTerm b
   | .bvar n      => .ok s!"X{lctx - n - 1}"
   | .lam _ _     => .error "transLamTerm :: unexpected lambda abstraction encountered in FOF translation"
   | .app _ t₁ t₂ =>
     if t₁.isForallE || t₁.isExistE then
       transQuantApp t₁ t₂ lctx
     else
-      match transLamTerm t₁ lctx, transLamTerm t₂ lctx with
-      | .ok t₁s, .ok t₂s => .ok s!"({t₁s} @ {t₂s})"
-      | .error e, _ => .error e
-      | .ok _, .error e => .error e
+      match t₁ with
+      | .app _ (.base b₁) t₁₂ =>
+        if isBiapp b₁ then
+          match transLamTerm t₁₂ lctx, transLamTerm t₂ lctx with
+          | .ok t₁s, .ok t₂s => .ok s!"({t₁s} {biappOp b₁} {t₂s})"
+          | .error e, _ => .error e
+          | .ok _, .error e => .error e
+        else
+          match transLamTerm t₁ lctx, transLamTerm t₂ lctx with
+          | .ok t₁s, .ok t₂s => .ok s!"({t₁s} @ {t₂s})"
+          | .error e, _ => .error e
+          | .ok _, .error e => .error e
+      | _ =>
+        match transLamTerm t₁ lctx, transLamTerm t₂ lctx with
+        | .ok t₁s, .ok t₂s => .ok s!"({t₁s} @ {t₂s})"
+        | .error e, _ => .error e
+        | .ok _, .error e => .error e
+  | .base b      => transLamBaseTerm b
 where
   expandQuantBody (s : LamSort) (t : LamTerm) : LamTerm :=
     match t with

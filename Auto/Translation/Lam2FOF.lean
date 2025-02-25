795432488bce5c4b8f39f9fb412bf0888e41a28c
Auguste Poiroux <augustepoiroux@gmail.com>
2025-02-26 00:04:50 +0100

    Send theorem as a sequent to TPTP instead of an axiom list and a conjecture

--- Auto/Translation/Lam2FOF.lean
+++ Auto/Translation/Lam2FOF.lean
@@ -8,19 +8,18 @@ def lam2FOF (lamVarTy : Array LamSort) (lamEVarTy : Array LamSort) (facts : Arra
   if (lamVarTy ++ lamEVarTy).any (fun s => s == .base .empty) then
     return "fof(empty_inhabited, axiom, $false)."
   let nFacts := facts.size
-  let factsStrs ← facts.zipWithIndex.mapM (fun (t, i) =>
-    if i == nFacts - 1 then
-      match t with
-      | .app _ (.base (.pcst .not)) t =>
-        match transLamTerm t with
-        | .ok ts => return s!"fof(c, conjecture, {ts})."
-        | .error e => throwError e
-      | _ => throwError "lam2FOF :: unexpected error"
-    else
+  -- intercalate all the facts except the last one in a sequent separated by commas
+  -- the last one is the negated conclusion
+  let hypothesesStr := String.intercalate ", " (← facts[0:nFacts - 1].toArray.toList.mapM (fun t =>
+    match transLamTerm t with
+    | .ok ts => pure ts
+    | .error e => throwError e))
+  let conclusionStr: String ← match facts[nFacts - 1]! with
+    | .app _ (.base (.pcst .not)) t =>
       match transLamTerm t with
-      | .ok ts =>
-        return s!"fof(a{i}, axiom, {ts})."
-      | .error e => throwError e)
-  return String.intercalate "\n" factsStrs.toList
+      | .ok ts => pure ts
+      | .error e => throwError e
+    | _ => throwError "lam2FOF :: conclusion is not negated"
+  return s!"fof(c, conjecture, [{hypothesesStr}] --> [{conclusionStr}])."
 
 end Auto
